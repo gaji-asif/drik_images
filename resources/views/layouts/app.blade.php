26e0db3d1ffddb62af4ef1128569d0f4473732b7
gaji-asif <anik200019@gmail.com>
2021-09-22 17:57:33 +0600

    all

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -25,7 +25,7 @@
             <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
                 <div class="container">
                     <a href="#" style="margin: 0 auto; font-size: 25px; font-weight: bold;">
-                        DRIK ADMIN
+                        DRIK ADMIN 
                     </a>
                     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                         <span class="navbar-toggler-icon"></span>
@@ -34,7 +34,7 @@
                     <div class="collapse navbar-collapse" id="navbarSupportedContent">
                         <!-- Left Side Of Navbar -->
 
-                        {{-- hello --}}
+                        
                         <ul class="navbar-nav mr-auto">
 
                         </ul>
